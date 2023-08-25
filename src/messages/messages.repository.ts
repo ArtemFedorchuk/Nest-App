b61c27ab77bd9f8f54f83b3f45ee2b6388fa42e8
ArtemFedorchuk <fedorchuk.zver@gmail.com>
2023-08-25 14:47:17 +0300

    init develop

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -10,9 +10,7 @@ export class MessagesRepository {
 
   async findAll() {
     const contents = await readFile('messages.json', 'utf8');
-    const messages = JSON.parse(contents);
-
-    return messages;
+    return JSON.parse(contents);
   }
 
   async create(content: string) {
